0d4da76cd042a7bd066b5a240d690d4dc65478fe
Aldon Manuel <ald19.mnl04@gmail.com>
2021-12-29 23:39:28 +0700

    Update database dan config

--- funcdash.py
+++ funcdash.py
@@ -2,12 +2,20 @@ import psycopg2
 
 import time
 
+import json
+
+# buka file JSON
+file_json = open("config/config.json")
+
+# prsing data JSON
+database = json.loads(file_json.read())
+
 # ==============================
 #       Connect to DB
 # ==============================
 
 def connectDB():
-    db_conn = psycopg2.connect("dbname=dashboard user=postgres password=pacific1904")
+    db_conn = psycopg2.connect(f"dbname={database['db_name']} user={database['user']} password={database['pass']} host={database['host']} port={database['port']}")
     cursor = db_conn.cursor()
     return (db_conn, cursor)
 
